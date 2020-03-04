b10c75fb91e8f8e6959003eb87a7e447909d1763
Javier <xavier.mcin@gmail.com>
2020-03-04 09:55:31 -0500

    finished on to next project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 //MOBILE MENU
 
-var mobileMenu = document.querySelector('#mobile-menu');
+let mobileMenu = document.querySelector('#mobile-menu');
+let mobileNav = document.querySelector('main #first header .header-container nav');
+let docBody = document.querySelector('body');
+let listItems = document.querySelector('main #first header .header-container nav > ul').getElementsByTagName('li');
+let newList = Object.values(listItems);
 
 mobileMenu.onclick = function() {
     if(this.classList == "open") {
         this.classList.remove('open');
+        mobileNav.classList.remove('clicked');
+        docBody.classList.remove('stick');
     } else {
         this.classList.add('open');
+        mobileNav.classList.add('clicked');
+        docBody.classList.add('stick');
     }
 };
+
+newList.forEach(function(curr) {
+    curr.onclick = function() {
+        if (curr.classList != "dropdown") {
+            if(mobileMenu.classList == "open") {
+                mobileMenu.classList.remove('open');
+                mobileNav.classList.remove('clicked');
+                docBody.classList.remove('stick');
+            }
+        }
+    }
+});
